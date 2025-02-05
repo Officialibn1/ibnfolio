d3e904172aa9fc16516afbc65b42f605acc5a75c
Isah Ibn Muhammad <officialibn001@gmail.com>
2025-02-05 18:38:45 +0100

    New ✨:Add mobile Navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
-import { Github, Linkedin, Twitter } from "lucide-react";
+import { Github, Linkedin, MenuIcon, Twitter } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import {
+	Sheet,
+	SheetContent,
+	SheetTrigger,
+	SheetTitle,
+} from "@/components/ui/sheet";
 
 export const Navbar = () => {
 	return (
@@ -14,7 +20,7 @@ export const Navbar = () => {
 					<AvatarFallback>IBN</AvatarFallback>
 				</Avatar>
 			</div>
-			<ul className='flex justify-center space-x-3 md:space-x-5 tracking-wider text-lg font-medium flex-1 '>
+			<ul className=' justify-center space-x-3 md:space-x-5 tracking-wider text-lg font-medium -1 hidden md:flex '>
 				<li>
 					<Link
 						href='/'
@@ -40,7 +46,7 @@ export const Navbar = () => {
 				</li>
 			</ul>
 
-			<ul className='flex justify-end space-x-2 flex-1'>
+			<ul className=' justify-end space-x-2 flex-1 hidden md:flex'>
 				<li className='border border-gray-800 rounded-full p-2 hover:text-indigo-700 hover:border-indigo-700  ease-in-out cursor-pointer'>
 					<Link
 						href={"https://www.linkedin.com/in/isah-muhammad-5046b125a/"}
@@ -65,6 +71,69 @@ export const Navbar = () => {
 					</Link>
 				</li>
 			</ul>
+
+			<div className='md:hidden'>
+				<Sheet>
+					<SheetTrigger>
+						<MenuIcon className='w-8 h-8' />
+					</SheetTrigger>
+
+					<SheetContent className='bg-foreground text-background h-screen flex flex-col'>
+						<SheetTitle className='hidden'>Navigation Menu</SheetTitle>
+						<ul className='flex flex-col justify-center gap-3 tracking-wider text-lg font-medium mt-10'>
+							<li>
+								<Link
+									href='/'
+									className='hover:text-indigo-700  ease-in-out'>
+									Home
+								</Link>
+							</li>
+
+							<li>
+								<Link
+									href='/#about-me'
+									className='hover:text-indigo-700  ease-in-out'>
+									About
+								</Link>
+							</li>
+
+							<li>
+								<Link
+									href='/projects'
+									className='hover:text-indigo-700  ease-in-out'>
+									Projects
+								</Link>
+							</li>
+						</ul>
+
+						<ul className='flex justify-end space-x-2 mt-auto'>
+							<li className='border border-gray-800 rounded-full p-2 hover:text-indigo-700 hover:border-indigo-700  ease-in-out cursor-pointer'>
+								<Link
+									href={"https://www.linkedin.com/in/isah-muhammad-5046b125a/"}
+									target='_Blank'>
+									<Linkedin />
+								</Link>
+							</li>
+
+							<li className='border border-gray-800 rounded-full p-2 hover:text-indigo-700 hover:border-indigo-700  ease-in-out cursor-pointer'>
+								<Link
+									href={"https://github.com/Officialibn1"}
+									target='_Blank'>
+									<Github />
+								</Link>
+							</li>
+
+							<li className='border border-gray-800 rounded-full p-2  hover:text-indigo-700 hover:border-indigo-700  ease-in-out cursor-pointer'>
+								<Link
+									href={"https://x.com/__ibn1/"}
+									target='_Blank'>
+									<Twitter />
+								</Link>
+							</li>
+						</ul>
+					</SheetContent>
+				</Sheet>
+			</div>
 		</nav>
 	);
 };
